82baa6c53e511848b4064c39751681b8df209d87
AndreyCorpse28090 <280990corpse@gmail.com>
2023-12-02 18:11:03 +0300

    Добавьте файлы проекта.

---
+++ 18_11_Bukov/18_11_Bukov.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <string>
+
+int evaluateExpression(const std::string& expression, size_t& index) 
+{
+    int result = 0;
+    int currentNumber = 0;
+    char currentOperator = '+';
+
+    while (index < expression.length()) 
+    {
+        char currentChar = expression[index++];
+
+        if (isdigit(currentChar)) 
+        {
+            currentNumber = currentNumber * 10 + (currentChar - '0');
+        }
+        else if (currentChar == '+' || currentChar == '-') 
+        {
+            result = (currentOperator == '+') ? result + currentNumber : result - currentNumber;
+            currentNumber = 0;
+            currentOperator = currentChar;
+        }
+        else if (currentChar == '(') 
+        {
+            currentNumber = evaluateExpression(expression, index);
+        }
+        else if (currentChar == ')')
+        {
+            break;
+        }
+    }
+
+    return (currentOperator == '+') ? result + currentNumber : result - currentNumber;
+}
+
+// Функция для вызова evaluateExpression
+int evaluateExpression(const std::string& expression)
+{
+    size_t index = 0;
+    return evaluateExpression(expression, index);
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "rus");
+    try 
+    {
+
+        std::string expression;
+        std::cout << "Введите математическое выражение: ";
+        std::getline(std::cin, expression);
+
+
+        int result = evaluateExpression(expression);
+        std::cout << "Результат: " << result << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Ошибка: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
